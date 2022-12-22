09417b36fffe3307c8816f0e1c154acfeea545c9
dushyant6 <dushyantptl6@gmail.com>
2022-12-21 19:57:57 -0500

    Add files via upload

---
+++ Wrapper.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import subprocess
+
+#subprocess.call('/Documents/CV_Fall22/Python Codes/CV_AutoSwap_P2/Face_Swap_Video.py')
+exec(open('code_files/Face_Swap_Video.py').read())
+exec(open('code_files/2face_swap_video_with_flickering.py').read())
+exec(open('code_files/tps_video.py').read())
+exec(open('code_files/2face_swap_video_TPS.py').read())
