a794a330d98c2dde8d3cced86f611e73ebe9a5a4
dushyant6 <dushyantptl6@gmail.com>
2022-12-21 19:58:40 -0500

    Update Wrapper.py

--- Wrapper.py
+++ Wrapper.py
@@ -3,7 +3,7 @@
 import subprocess
 
 #subprocess.call('/Documents/CV_Fall22/Python Codes/CV_AutoSwap_P2/Face_Swap_Video.py')
-exec(open('code_files/Face_Swap_Video.py').read())
-exec(open('code_files/2face_swap_video_with_flickering.py').read())
-exec(open('code_files/tps_video.py').read())
-exec(open('code_files/2face_swap_video_TPS.py').read())
+exec(open('scripts/Face_Swap_Video.py').read())
+exec(open('scripts/2face_swap_video_with_flickering.py').read())
+exec(open('scripts/tps_video.py').read())
+exec(open('scripts/2face_swap_video_TPS.py').read())
